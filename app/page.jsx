cfb6f6e267377471fb19b8da88d67c92244a2a96
debashisgarai18 <debashis1805@gmail.com>
2025-06-02 01:09:47 +0530

    added another component

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
 import { CarCard } from "@/components/car-card";
 import { HomeSearch } from "@/components/home-search";
 import { Button } from "@/components/ui/button";
-import { featuredCars } from "@/lib/dummyData";
+import { carMakes, featuredCars } from "@/lib/dummyData";
 import { ChevronRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -36,8 +37,8 @@ const HeroSection = () => {
             <h2 className="text-2xl font-bold ">Featured Cars</h2>
             <Button variant="ghost" className="flex items-center" asChild>
               <Link href="/cars">
-              View All
-              <ChevronRight className="h-4 w-4 ml-1" />
+                View All
+                <ChevronRight className="h-4 w-4 ml-1" />
               </Link>
             </Button>
           </div>
@@ -45,6 +46,38 @@ const HeroSection = () => {
             {featuredCars.map((ele) => {
               return <CarCard key={ele.id} car={ele} />;
             })}
+          </div>
+        </div>
+      </section>
+      <section className="py-12 bg-gray-50">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-center mb-8">
+            <h2 className="text-2xl font-bold ">Browse by brands</h2>
+            <Button variant="ghost" className="flex items-center" asChild>
+              <Link href="/cars">
+                View All
+                <ChevronRight className="h-4 w-4 ml-1" />
+              </Link>
+            </Button>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {carMakes.map((make) => (
+              <Link
+                key={make.name}
+                href={`/cars?make=${make.name}`}
+                className="bg-white rounded-lg shadow p-4 text-center hover:shadow-md transition cursor-pointer"
+              >
+                <div className="h-16 w-auto m-auto mb-2 relative">
+                  <Image
+                    src={make.image}
+                    alt={make.name}
+                    fill
+                    style={{ objectFit: "contain" }}
+                  />
+                </div>
+                <h3 className="font-medium">{make.name}</h3>
+              </Link>
+            ))}
           </div>
         </div>
       </section>
