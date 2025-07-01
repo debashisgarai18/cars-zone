8acda4fc1a27ba8fb8a1dec12cc0711b185b6972
debashisgarai18 <debashis1805@gmail.com>
2025-07-01 12:19:32 +0530

    search bar UI added

--- components/home-search.jsx
+++ components/home-search.jsx
@@ -1,3 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Camera } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export const HomeSearch = () => {
-    return <div>This is search</div>
-}
+  const [searchText, setSearchText] = useState("");
+  const [isImgSearchActive, setIsImgSearchActive] = useState(false);
+
+  const handleFormSubmit = () => {};
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <div className="relative flex items-center">
+          <Input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Enter model or Use the AI Image Search.."
+            className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm"
+          />
+          <div className="absolute right-[100px]">
+            <Camera
+              size={35}
+              onClick={() => setIsImgSearchActive(!isImgSearchActive)}
+              className="cursor-pointer rounded-xl p-1.5"
+              style={{
+                background: isImgSearchActive ? "black" : "",
+                color: isImgSearchActive ? "white" : "",
+              }}
+            />
+          </div>
+          <Button type="submit" className="absolute right-2 rounded-full">
+            Search
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
