3136dda6fc27c67f9ccc51e2c779cb099ade1b7d
debashisgarai18 <debashis1805@gmail.com>
2025-08-10 22:03:15 +0530

    WIP: adding UI sidebar to admin

--- app/(admin)/admin/_components/sidebar.js
+++ app/(admin)/admin/_components/sidebar.js
@@ -1,7 +1,58 @@
-"use client"
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Calendar, Car, Cog, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/admin",
+  },
+  {
+    label: "Cars",
+    icon: Car,
+    href: "/admin/cars",
+  },
+  {
+    label: "Test Drivers",
+    icon: Calendar,
+    href: "/admin/test-drivers",
+  },
+  {
+    label: "Settings",
+    icon: Cog,
+    href: "/admin/settings",
+  },
+];
 
 export default function Sidebar() {
-    return <div>
-        Sidebar
-    </div>
+  const pathName = usePathname();
+  return (
+    <>
+      {/* for web */}
+      <div className="hidden md:flex h-full flex-col overflow-y-auto bg-white shadow-sm">
+        {routes.map((elem) => {
+          return (
+            <Link
+              key={elem.href}
+              href={elem.href}
+              className={cn(
+                "flex items-center gap-x-2 text-slate-500 text-sm font-medium pl-6 transition-all hover:text-slate-600 hover:bg-slate-100/50 h-12",
+                pathName === elem.href &&
+                  "text-blue-700 bg-blue-100/50 hohver:bg-blue-100 hover:text-blue-700"
+              )}
+            >
+              <elem.icon className="h-5 w-5" />
+              {elem.label}
+            </Link>
+          );
+        })}
+      </div>
+      {/* for mobile */}
+      <div></div>
+    </>
+  );
 }
--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import { notFound } from "next/navigation";
 import Sidebar from "./_components/sidebar";
 
-export default async function AdminLayout() {
+export default async function AdminLayout({ children }) {
   const admin = await getAdmin();
 
   if (!admin.authorized) {
@@ -12,9 +12,10 @@ export default async function AdminLayout() {
   return (
     <div className="h-full">
       <Header isAdminPage={true} />
-      <div className="flex h-full w-56 flex-col top-20 insert-y-0 z-50 fixed">
+      <div className="flex h-full w-56 flex-col top-20 inset-y-0 z-50 fixed">
         <Sidebar />
       </div>
+      <main className="md:pl-56 pt-[80px] h-full">{children}</main>
     </div>
   );
 }
