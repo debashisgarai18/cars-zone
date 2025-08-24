2d6a6d565123e78f6a4a4838536304ce2eeca086
debashisgarai18 <debashis1805@gmail.com>
2025-08-24 19:36:28 +0530

    added car-list

--- app/(admin)/admin/cars/_components/cars-list.jsx
+++ app/(admin)/admin/cars/_components/cars-list.jsx
@@ -1,18 +1,84 @@
 "use client";
 
+import { deleteCar, getCars, updateCarStatus } from "@/actions/cars";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Plus, Search } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import useFetch from "@/hooks/use-fetch";
+import { formatCurrency } from "@/lib/helper";
+import { Badge, CarIcon, Loader2, Plus, Search } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CarsList() {
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState("");
 
   const router = useRouter();
 
+  // getCar server action
+  const {
+    loading: loadingCars,
+    fn: fetchCarsFunction,
+    data: carsData,
+    error: carsError,
+  } = useFetch(getCars);
+
+  useEffect(() => {
+    fetchCarsFunction(search);
+  }, [search]);
+
+  // deleteCar server action
+  const {
+    loading: deletingCar,
+    fn: deleteCarsFunction,
+    data: dataResult,
+    error: deleteError,
+  } = useFetch(deleteCar);
+
+  // updateCar server action
+  const {
+    loading: updatingCar,
+    fn: updateCarStatusFunction,
+    data: updateResult,
+    error: updateError,
+  } = useFetch(updateCarStatus);
+
+  const getStatusBadge = (status) => {
+    switch (status) {
+      case "AVAILABLE":
+        return (
+          <Badge className="bg-green-100 text-green-800 hover:bg-green-100">
+            Available
+          </Badge>
+        );
+      case "UNAVAILABLE":
+        return (
+          <Badge className="bg-amber-100 text-amber-800 hover:bg-amber-100">
+            Unavailable
+          </Badge>
+        );
+      case "SOLD":
+        return (
+          <Badge className="bg-blue-100 text-blue-800 hover:bg-blue-100">
+            Sold
+          </Badge>
+        );
+      default:
+        return <Badge variabnt="outline">{status}</Badge>;
+    }
+  };
+
   const handleSearchSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // api call to be added
   };
 
@@ -39,7 +105,63 @@ export default function CarsList() {
           </div>
         </form>
       </div>
-      {/* cars table to be rendered */}
+      <Card>
+        <CardContent className="p=0">
+          {loadingCars && !carsData ? (
+            <div className="flex justify-center items-center py-12">
+              <Loader2 className="h-8 w-8 animate-spin text-gray-400" />
+            </div>
+          ) : carsData?.success && carsData.data.length > 0 ? (
+            <div className="overflow-x-auto">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-12"></TableHead>
+                    <TableHead>Make and Model</TableHead>
+                    <TableHead>Year</TableHead>
+                    <TableHead>Price</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Featured</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {carsData.data.map((car) => {
+                    return (
+                      <TableRow key={car.id}>
+                        <TableCell className="w-10 h-10 rounded-md overflow-hidden">
+                          {car.images && car.images.length > 0 ? (
+                            <Image
+                              src={car.images[0]}
+                              alt={`${car.make} ${car.model}`}
+                              height={40}
+                              width={40}
+                              className="w-full h-full object-cover"
+                              priority
+                            />
+                          ) : (
+                            <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+                              <CarIcon className="h-6 w-6 text-gray-400" />
+                            </div>
+                          )}
+                        </TableCell>
+                        <TableCell className="font-medium">
+                          {car.model} {car.make}
+                        </TableCell>
+                        <TableCell>{car.year}</TableCell>
+                        <TableCell>{formatCurrency(car.price)}</TableCell>
+                        <TableCell>{car.status}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </CardContent>
+      </Card>
     </div>
   );
 }
