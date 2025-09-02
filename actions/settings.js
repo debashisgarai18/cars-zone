39661fb1acfad876e344ffeead655bdaa581c547
debashisgarai18 <debashis1805@gmail.com>
2025-09-03 00:25:34 +0530

    wip : adding settings server action

---
+++ actions/settings.js
@@ -0,0 +1,26 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getDealershipInfo() {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) throw new Error("User not found");
+
+    // get the dealership records
+    let dealership = await db.dealershipInfo.findFirst({
+      include: {
+        workingHours: {
+          orderBy: {
+            dayOfWeek: "asc",
+          },
+        },
+      },
+    });
+  } catch (error) {}
+}
