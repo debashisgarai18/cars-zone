d602fa2a477646319fcd9aafa4dbfc337d2d4ce8
debashisgarai18 <debashis1805@gmail.com>
2025-09-03 12:50:14 +0530

    wip : added the server action for settings

--- actions/settings.js
+++ actions/settings.js
@@ -1,5 +1,8 @@
+"use server";
+
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function getDealershipInfo() {
   try {
@@ -22,5 +25,187 @@ export async function getDealershipInfo() {
         },
       },
     });
-  } catch (error) {}
+
+    if (!dealership) {
+      dealership = await db.dealershipInfo.create({
+        data: {
+          workingHours: {
+            create: [
+              {
+                dayOfWeek: "MONDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: true,
+              },
+              {
+                dayOfWeek: "TUESDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: true,
+              },
+              {
+                dayOfWeek: "WEDNESDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: true,
+              },
+              {
+                dayOfWeek: "THURSDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: true,
+              },
+              {
+                dayOfWeek: "FRIDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: true,
+              },
+              {
+                dayOfWeek: "SATURDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: true,
+              },
+              {
+                dayOfWeek: "SUNDAY",
+                openTime: "09:00",
+                closeTime: "18:00",
+                isOpen: false,
+              },
+            ],
+          },
+        },
+        include: {
+          workingHours: {
+            orderBy: {
+              dayOfWeek: "asc",
+            },
+          },
+        },
+      });
+    }
+
+    return {
+      success: true,
+      data: {
+        ...dealership,
+        createdAt: dealership.createdAt.toISOString(),
+        updatedAt: dealership.updatedAt.toISOString(),
+      },
+    };
+  } catch (error) {
+    throw new Error(`Error fetching dealership info: ${error.message}`);
+  }
+}
+
+export async function saveWorkingHours(workingHours) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user || user.role !== "ADMIN")
+      throw new Error("Unauthorized : Admin access required");
+
+    const dealership = await db.dealershipInfo.findFirst();
+
+    if (!dealership) {
+      throw new Error("Dealership Info not found");
+    }
+
+    await db.dealershipInfo.deleteMany({
+      where: {
+        dealershipId: dealership.id,
+      },
+    });
+
+    // creating new working hours
+    for (const hr of workingHours) {
+      await db.workingHour.create({
+        data: {
+          dayOfWeek: hr.dayOfWeek,
+          openTime: hr.openTime,
+          closeTime: hr.closeTime,
+          isOpen: hr.isOpen,
+          dealershipId: dealership.id,
+        },
+      });
+    }
+
+    revalidatePath("/admin/settings");
+    revalidatePath("/");
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    throw new Error(`Error saving working hours: ${error.message}`);
+  }
+}
+
+export async function getUsers() {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user || user.role !== "ADMIN")
+      throw new Error("Unauthorized : Admin access required");
+
+    const users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return {
+      success: true,
+      data: users.map((user) => {
+        return {
+          ...user,
+          createdAt: user.createdAt.toISOString(),
+          updatedAt: user.updatedAt.toISOString(),
+        };
+      }),
+    };
+  } catch (error) {
+    throw new Error(`Error fetching users: ${error.message}`);
+  }
+}
+
+export async function updateUserRole(userId, role) {
+  try {
+    const { userId: adminId } = await auth();
+    if (!adminId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: adminId },
+    });
+
+    if (!user || user.role !== "ADMIN")
+      throw new Error("Unauthorized : Admin access required");
+
+    // update the user here
+    await db.user.update({
+      where: {
+        id: userId,
+      },
+      data: { role },
+    });
+
+    revalidatePath("/admin/settings");
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    throw new Error(`Error updating user role: ${error.message}`);
+  }
 }
