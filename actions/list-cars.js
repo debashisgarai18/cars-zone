4680fafbbe080f90aa458d48b596b29d429eaeed
debashisgarai18 <debashis1805@gmail.com>
2025-09-20 21:07:44 +0530

    wip: added the server action for car details

--- actions/list-cars.js
+++ actions/list-cars.js
@@ -4,6 +4,7 @@ import { serializedCarData } from "@/lib/helper";
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
+import { id } from "zod/v4/locales";
 
 export async function getCarFilters() {
   try {
@@ -229,7 +230,7 @@ export async function toggleSavedCar(carId) {
   }
 }
 
-export async function gteSavedCars() {
+export async function getSavedCars() {
   try {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
@@ -271,5 +272,99 @@ export async function gteSavedCars() {
       success: false,
       error: error.message,
     };
+  }
+}
+
+export async function getCarById(carId) {
+  try {
+    const { userId } = await auth();
+    let dbUser = null;
+
+    if (userId) {
+      dbUser = await db.user.findUnique({
+        where: {
+          clerkUserId: userId,
+        },
+      });
+    }
+
+    const car = await db.car.findUnique({
+      where: {
+        id: carId,
+      },
+    });
+
+    if (!car) {
+      return {
+        success: false,
+        error: "Car not found",
+      };
+    }
+
+    let isWishlisted = false;
+    if (dbUser) {
+      const savedCar = await db.userSavedCar.findUnique({
+        where: {
+          userId_carId: {
+            userId: dbUser.id,
+            carId,
+          },
+        },
+      });
+      isWishlisted = !!savedCar;
+    }
+
+    const existingTestDrive = await db.testDriveBooking.findFirst({
+      where: {
+        carId,
+        userId: dbUser?.id,
+        status: { in: ["PENDING", "CONFIRMED", "COMPLETED"] },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    let userTestDrive = null;
+
+    if (existingTestDrive) {
+      userTestDrive = {
+        id: existingTestDrive.id,
+        status: existingTestDrive.status,
+        bookingDate: existingTestDrive.bookingDate.toISOString(),
+      };
+    }
+
+    const dealership = await db.dealershipInfo.findFirst({
+      include: {
+        workingHours: true,
+      },
+    });
+
+    return {
+      success: true,
+      data: {
+        ...serializedCarData(car, isWishlisted),
+        testDriveInfo: {
+          userTestDrive,
+          dealership: dealership
+            ? {
+                ...dealership,
+                createdAt: dealership.createdAt.toISOString(),
+                updatedAt: dealership.updatedAt.toISOString(),
+                workingHours: dealership.workingHours.map((wh) => {
+                  return {
+                    ...wh,
+                    createdAt: wh.createdAt.toISOString(),
+                    updatedAt: wh.updatedAt.toISOString(),
+                  };
+                }),
+              }
+            : null,
+        },
+      },
+    };
+  } catch (error) {
+    throw new Error(`Error fetching car details by id : ${error.message}`);
   }
 }
